493e9421a4569bd5effb8eb88fa3a49b287da9c7
Serafin Alferez <malaga.carbon3@gmail.com>
2023-12-04 19:10:38 +0100

    Add media.files, entities, repos

---
+++ src/repos/users/users.mongo.repo.ts
@@ -0,0 +1,47 @@
+import createDebug from 'debug';
+import { Repository } from '../repo.js';
+import { LoginUser, User } from '../../entities/user.js';
+import { UserModel } from './users.mongo.model.js';
+import { HttpError } from '../../types/http.error.js';
+import { Auth } from '../../services/auth.js';
+
+const debug = createDebug('W7E:users:mongo:repo');
+
+export class UsersMongoRepo implements Repository<User> {
+  constructor() {
+    debug('Instantiated');
+  }
+
+  async create(newItem: Omit<User, 'id'>): Promise<User> {
+    newItem.passwd = await Auth.hash(newItem.passwd);
+    const result: User = await UserModel.create(newItem);
+    return result;
+  }
+
+  async login(LoginUser: LoginUser): Promise<User> {
+    debug('login email', LoginUser.email)
+    const result = await UserModel.findOne({ email: LoginUser.email }).exec();
+    if (!result || !(await Auth.compare(LoginUser.passwd, result.passwd)))
+      throw new HttpError(401, 'Unauthorized');
+    return result;
+  }
+
+  async getAll(): Promise<User[]> {
+    const result = await UserModel.find().exec();
+    return result;
+  }
+
+  async getById(id: string): Promise<User> {
+    const result = await UserModel.findById(id).exec();
+    if (!result) throw new HttpError(404, 'Not Found', 'GetById nos possible');
+    return result;
+  }
+
+  async update(id: string, updatedItem: Partial<User>): Promise<User> {
+    const result = await UserModel.findByIdAndUpdate(id, updatedItem,{
+      new: true,
+    }).exec();
+    if (!result) throw new HttpError(404, 'Not Found', 'Update not possible');
+    return result;
+  }
+}
---
+++ src/types/media.files.ts
@@ -0,0 +1,5 @@
+import { v2 as cloudinary } from 'cloudinary';
+import createDebug from 'debug';
+import { ImgData } from './img.data.js';
+import { HttpError } from './http.error.js';
+
